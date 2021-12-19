844b6ec9c36185c985ca031366baea2b25665b18
Noah H <noahholo@icloud.com>
2021-12-19 16:29:06 -0600

    Add questions, leaderboard, and state models to Game model
    And add started state

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,16 +1,25 @@
 import Settings from './settings';
 import Player from './player';
+import State from './state';
 
 export default class Game {
 
   private _owner: string;
   private _settings: Settings;
   private _players: Player[];
+  private _questions: Map<number, string[]>;
+  private _started: boolean;
+  private _leaderboard: Map<string, number>;
+  private _state: State;
 
   constructor() {
     this._owner = '';
     this._settings = new Settings();
     this._players = [];
+    this._questions = new Map<number, string[]>();
+    this._started = false;
+    this._leaderboard = new Map<string, number>();
+    this._state = new State();
   }
 
   fromJSON(json: any): Game {
@@ -20,6 +29,14 @@ export default class Game {
     for ( const player in json.players ) {
       game.players.push(new Player().fromJSON(json.players[player]));
     }
+    Object.keys(json.questions ?? []).forEach((key) => {
+      game.questions.set(Number(key), json.questions[key]);
+    });
+    game.started = json.started ?? false;
+    Object.keys(json.leaderboard ?? []).forEach((key) => {
+      game.leaderboard.set(key, json.leaderboard[key]);
+    });
+    game.state = json.state ? new State().fromJSON(json.state) : new State();
     return game;
   }
 
@@ -52,6 +69,38 @@ export default class Game {
 
   set players(value: Player[]) {
     this._players = value;
+  }
+
+  get questions(): Map<number, string[]> {
+    return this._questions;
+  }
+
+  set questions(value: Map<number, string[]>) {
+    this._questions = value;
+  }
+
+  get started(): boolean {
+    return this._started;
+  }
+
+  set started(value: boolean) {
+    this._started = value;
+  }
+
+  get leaderboard(): Map<string, number> {
+    return this._leaderboard;
+  }
+
+  set leaderboard(value: Map<string, number>) {
+    this._leaderboard = value;
+  }
+
+  get state(): State {
+    return this._state;
+  }
+
+  set state(value: State) {
+    this._state = value;
   }
 
 }
