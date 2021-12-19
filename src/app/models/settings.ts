bc2ec6e2c0ecd7f1c11be988183e4677053dbbad
Noah H <noahholo@icloud.com>
2021-12-19 16:29:23 -0600

    Add bout count to the settings model

--- src/app/models/settings.ts
+++ src/app/models/settings.ts
@@ -2,16 +2,19 @@ export default class Settings {
 
   private _discussionTime: number;
   private _peaceBouts: number;
+  private _bouts: number;
 
   constructor() {
     this._discussionTime = 3;
     this._peaceBouts = 1;
+    this._bouts = 5;
   }
 
   fromJSON(json: any): Settings {
     const settings: Settings = new Settings();
     settings.discussionTime = json.discussionTime ?? 3;
     settings.peaceBouts = json.peaceBouts ?? 1;
+    settings.bouts = json.bouts ?? 5;
     return settings;
   }
 
@@ -19,7 +22,8 @@ export default class Settings {
     return {
       discussionTime: this.discussionTime,
       peaceBouts: this.peaceBouts,
-    }
+      bouts: this.bouts,
+    };
   }
 
   get discussionTime(): number {
@@ -36,6 +40,14 @@ export default class Settings {
 
   set peaceBouts(value: number) {
     this._peaceBouts = value;
+  }
+
+  get bouts(): number {
+    return this._bouts;
+  }
+
+  set bouts(value: number) {
+    this._bouts = value;
   }
 
 }
