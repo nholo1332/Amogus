8461702045cff8e8ff1f750e1400c782c4e0ef57
Noah H <noahholo@icloud.com>
2021-12-19 16:30:24 -0600

    Create game setup function
    To save all data to database and choose imposter

--- src/app/providers/databaseProvider.ts
+++ src/app/providers/databaseProvider.ts
@@ -3,6 +3,8 @@ import {AngularFireDatabase} from '@angular/fire/compat/database';
 import firebase from 'firebase/compat';
 import {customAlphabet} from 'nanoid/async';
 import {AuthService} from '../services/authServices';
+import Question from '../models/question';
+import Game from '../models/game';
 
 @Injectable()
 export class DatabaseProvider {
@@ -56,6 +58,28 @@ export class DatabaseProvider {
       });
       const shuffled = questions.sort(() => 0.5 - Math.random());
       return shuffled.slice(0, count);
+    });
+  }
+
+  public setupGame(questions: Question[], game: Game, key: string): Promise<void> {
+    const ids: string[] = questions.map((question) => question.id);
+    const leaderboard: Map<string, number> = game.players.reduce((map: any, obj) => {
+      map[obj.uid] = 0;
+      return map;
+    }, {});
+    game.state.round = 1;
+    game.state.discussionTimeRemaining = game.settings.discussionTime;
+    let imposter: string = game.players[Math.floor(Math.random() * game.players.length)].uid;
+    return this.db.ref('games').child(key).child('players').child(imposter).child('imposter').set(true).then(() => {
+      return this.db.ref('games').child(key).update({
+        questions: {
+          1: ids,
+        },
+        settings: game.settings.toJSON(),
+        started: true,
+        leaderboard: leaderboard,
+        state: game.state.toJSON(),
+      });
     });
   }
 
