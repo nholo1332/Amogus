f0d8b861ef63f5a06757bbecddd376bd654d9509
Noah H <noahholo@icloud.com>
2021-12-18 23:03:17 -0600

    Add route for game view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {JoinGameComponent} from './views/join-game/join-game.component';
+import {GameComponent} from './views/game/game.component';
 
 const routes: Routes = [
   { path: 'join', component: JoinGameComponent },
+  { path: 'game/:key', component: GameComponent },
   { path: '', redirectTo: '/join', pathMatch: 'full' },
   { path: '**', redirectTo: '/join', pathMatch: 'full' },
 ];
