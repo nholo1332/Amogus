0952b572652cff48a9ef1be2606b5b1ba827a9d0
Noah H <noahholo@icloud.com>
2021-12-18 22:32:23 -0600

    Create databaseProvider.ts

---
+++ src/app/providers/databaseProvider.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {AngularFireDatabase} from '@angular/fire/compat/database';
+import firebase from 'firebase/compat';
+
+@Injectable()
+export default class DatabaseProvider {
+
+  private db: firebase.database.Database;
+
+  constructor(private _db: AngularFireDatabase) {
+    this.db = _db.database;
+  }
+
+  public isGameJoinable(key: string): Promise<boolean> {
+    return this.db.ref('games').child(key).once('value').then((data) => {
+      return data.exists() && !data.child('started').val();
+    });
+  }
+
+}
