19ccc4dde55d2dc9d170c6abba5d0d91948e4c4c
Noah H <noahholo@icloud.com>
2021-12-19 20:07:48 -0600

    Update player-game-view.component.ts

--- src/app/components/player-game-view/player-game-view.component.ts
+++ src/app/components/player-game-view/player-game-view.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import Game from '../../models/game';
 
 @Component({
   selector: 'app-player-game-view',
   templateUrl: './player-game-view.component.html',
-  styleUrls: ['./player-game-view.component.scss']
+  styleUrls: ['./player-game-view.component.scss'],
 })
 export class PlayerGameViewComponent implements OnInit {
 
-  constructor() { }
+  @Input() game: Game = new Game();
+  @Input() gameKey: string = '';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
