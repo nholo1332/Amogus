3d040a4a833504e4429f87c96ce16b71fcd28aa4
Noah H <noahholo@icloud.com>
2021-12-31 11:28:15 -0600

    Sort players by their leaderboard ranking value
    And create function to return mat icon for 1st - 3rd places

--- src/app/dialog/rankings-display-dialog/rankings-display-dialog.component.ts
+++ src/app/dialog/rankings-display-dialog/rankings-display-dialog.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {rankingsDisplayDialogData} from '../dialogData';
+import Player from '../../models/player';
 
 @Component({
   selector: 'app-rankings-display-dialog',
@@ -9,11 +10,27 @@ import {rankingsDisplayDialogData} from '../dialogData';
 })
 export class RankingsDisplayDialogComponent implements OnInit {
 
+  players: Player[] = [];
+
   constructor(public dialogRef: MatDialogRef<RankingsDisplayDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: rankingsDisplayDialogData) {
+    this.players = data.game.players.sort((a, b) => ( data.game.leaderboard.get(b.uid) ?? 0 ) - ( data.game.leaderboard.get(a.uid) ?? 0 ));
   }
 
   ngOnInit(): void {
+  }
+
+  getPlacing(index: number): string {
+    switch ( index + 1 ) {
+      case 1:
+        return 'filter_1';
+      case 2:
+        return 'filter_2';
+      case 3:
+        return 'filter_3';
+      default:
+        return '';
+    }
   }
 
 }
