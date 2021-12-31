2766db8b0169d88e8f526321a02fbe05a658c53a
Noah H <noahholo@icloud.com>
2021-12-30 23:38:24 -0600

    Setup RankingsDialog MatDialog

--- src/app/dialog/rankings-display-dialog/rankings-display-dialog.component.ts
+++ src/app/dialog/rankings-display-dialog/rankings-display-dialog.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {rankingsDisplayDialogData} from '../dialogData';
 
 @Component({
   selector: 'app-rankings-display-dialog',
   templateUrl: './rankings-display-dialog.component.html',
-  styleUrls: ['./rankings-display-dialog.component.scss']
+  styleUrls: ['./rankings-display-dialog.component.scss'],
 })
 export class RankingsDisplayDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<RankingsDisplayDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: rankingsDisplayDialogData) {
+  }
 
   ngOnInit(): void {
   }
