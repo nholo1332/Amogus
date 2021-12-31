80ff09a2f43a5de0aca74ecfa1d65dc401247aa0
Noah H <noahholo@icloud.com>
2021-12-30 23:32:29 -0600

    Create rankings dialog

---
+++ src/app/dialog/rankings-display-dialog/rankings-display-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rankings-display-dialog',
+  templateUrl: './rankings-display-dialog.component.html',
+  styleUrls: ['./rankings-display-dialog.component.scss']
+})
+export class RankingsDisplayDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
